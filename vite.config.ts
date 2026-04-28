cf8ed8de6dad3de295bc43605b5e80e0fcd83cfa
selfishtom <toomoor@yahoo.com>
2026-04-28 20:06:09 +0330

    edit vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter()],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+    },
+  },
   server: {
     host: "127.0.0.1",
     port: 5173,
